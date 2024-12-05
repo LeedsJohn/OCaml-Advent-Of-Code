0b87325969d7480d1f209cbc2b87351bc0f3369c
John Leeds <leedsjohn6@gmail.com>
2024-12-05 14:24:17 -0800

    finish

--- lib/solutions/problem_2021_20.ml
+++ lib/solutions/problem_2021_20.ml
@@ -2,78 +2,65 @@ open! Core
 open! Helpers
 
 module Game = struct
-  type t = { alg : string; board : Set.M(Coordinate).t }
+  type t = { alg : string; board : char Board.t; default : char }
 
   let bin_to_dec s =
-    print_endline s;
     String.foldi (String.rev s) ~init:0 ~f:(fun i acc c ->
         if Char.(c = '#') then acc + Int.pow 2 i else acc)
 
-  let get { board; _ } pos = if Set.mem board pos then '#' else '.'
+  let get { board; default; _ } pos =
+    Map.find board pos |> Option.value ~default
 
   let get_alg_index t (x, y) =
     List.map
       (List.range (y - 1) (y + 2))
       ~f:(fun y ->
-        List.map
-          (List.range (x - 1) (x + 2))
-          ~f:(fun x ->
-            print_s [%sexp ((x, y, get t (x, y)) : int * int * char)];
-            get t (x, y)))
+        List.map (List.range (x - 1) (x + 2)) ~f:(fun x -> get t (x, y)))
     |> List.join |> String.of_list |> bin_to_dec
 
-  let min_coordinates { board; _ } =
-    Set.fold board ~init:(Int.max_value, Int.max_value)
-      ~f:(fun (x, y) (x', y') -> (Int.min x x', Int.min y y'))
-
-  let max_coordinates { board; _ } =
-    Set.fold board ~init:(Int.min_value, Int.min_value)
-      ~f:(fun (x, y) (x', y') -> (Int.max x x', Int.max y y'))
-
-  let print_board t =
-    let min_x, min_y = min_coordinates t in
-    let max_x, max_y = max_coordinates t in
-    print_s [%sexp ((min_x, min_y) : int * int)];
-    print_s [%sexp ((max_x, max_y) : int * int)];
+  let _print_board t =
+    let min_x, min_y = Board.min_coordinates t.board in
+    let max_x, max_y = Board.max_coordinates t.board in
     List.iter
       (List.range min_y (max_y + 1))
       ~f:(fun y ->
         List.map (List.range min_x (max_x + 1)) ~f:(fun x -> get t (x, y))
         |> String.of_list |> print_endline)
 
-  let step ({ alg; _ } as t) =
-    let min_x, min_y = min_coordinates t in
-    let max_x, max_y = max_coordinates t in
+  let step ({ alg; default; board } as t) =
+    let min_x, min_y = Board.min_coordinates board in
+    let max_x, max_y = Board.max_coordinates board in
     let new_board =
       List.cartesian_product
-        (List.range (min_x - 3) (max_x + 4))
-        (List.range (min_y - 3) (max_y + 4))
+        (List.range (min_x - 2) (max_x + 2))
+        (List.range (min_y - 2) (max_y + 2))
       |> List.fold
-           ~init:(Set.empty (module Coordinate))
+           ~init:(Map.empty (module Coordinate))
            ~f:(fun acc pos ->
              let alg_index = get_alg_index t pos in
-             match String.get alg alg_index with
-             | '#' -> Set.add acc pos
-             | _ -> acc)
+             Map.add_exn acc ~key:pos ~data:(String.get alg alg_index))
     in
-    { t with board = new_board }
+    {
+      t with
+      board = new_board;
+      default = (if Char.(default = '#') then '.' else '#');
+    }
 
   let of_string s =
     let alg = String.split_lines s |> List.hd_exn in
     let board =
-      String.drop_prefix s (String.length alg + 1)
-      |> Board.of_string
-      |> Map.filter ~f:(Char.equal '#')
-      |> Map.key_set
+      String.drop_prefix s (String.length alg + 1) |> Board.of_string
     in
-    { alg; board }
+    { alg; board; default = '.' }
 end
 
 let part1 s =
-  let game = Game.of_string s |> Game.step in
-  (* let game = Game.of_string s in *)
-  Game.print_board game;
-  (* print_s [%sexp (Game.get_alg_index game (2, 3) : int)]; *)
-  Set.length game.board |> Int.to_string |> Ok
+  let game = Game.of_string s |> Game.step |> Game.step in
+  Map.count game.board ~f:(Char.equal '#') |> Int.to_string |> Ok
 
-let part2 _ = Error (Error.of_string "Unimplemented")
+let part2 s =
+  let game =
+    List.fold (List.range 0 50) ~init:(Game.of_string s) ~f:(fun acc _ ->
+        Game.step acc)
+  in
+  Map.count game.board ~f:(Char.equal '#') |> Int.to_string |> Ok
