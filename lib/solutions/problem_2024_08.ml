fa7a7fab085cc0ef3d819c51aaf757e355f9e24d
John Leeds <leedsjohn6@gmail.com>
2024-12-09 15:49:00 -0800

    stuff

--- lib/solutions/problem_2024_08.ml
+++ lib/solutions/problem_2024_08.ml
@@ -1,4 +1,4 @@
- open! Core
+open! Core
 
 let part1 _ = Error (Error.of_string "Unimplemented")
-let part2 _ = Error (Error.of_string "Unimplemented") 
+let part2 _ = Error (Error.of_string "Unimplemented")
--- lib/solutions/problem_2024_09.ml
+++ lib/solutions/problem_2024_09.ml
@@ -1,11 +1,31 @@
 open! Core
 
 module Entry = struct
-  type t = Empty of int | Full of int * int
+  type t = Empty of int | Full of int * int [@@deriving sexp]
 end
 
 module Entry_list = struct
-  type t = Entry.t list
+  type t = Entry.t list [@@deriving sexp]
+
+  let fix_stuff t =
+    let rec aux acc = function
+      | [] -> List.rev acc
+      | (Entry.Empty n1 as hd) :: tl -> (
+          match acc with
+          | Entry.Empty n2 :: tl2 -> aux (Empty (n1 + n2) :: tl2) tl
+          | _ -> aux (hd :: acc) tl)
+      | (Full (n1, id1) as hd) :: tl -> (
+          match acc with
+          | Entry.Full (n2, id2) :: tl2 ->
+              if id1 = id2 then aux (Full (n1 + n2, id1) :: tl2) tl
+              else aux (hd :: acc) tl
+          | _ -> aux (hd :: acc) tl)
+    in
+    let t = aux [ List.hd_exn t ] (List.tl_exn t) in
+    List.filter t ~f:(function Empty 0 | Full (0, _) -> false | _ -> true)
+    |> List.rev
+    |> List.drop_while ~f:(function Entry.Empty _ -> true | _ -> false)
+    |> List.rev
 
   let of_string s : t =
     let rec aux acc i id =
@@ -19,16 +39,9 @@ module Entry_list = struct
           (Entry.Empty (Char.get_digit_exn (String.get s i)) :: acc)
           (i + 1) id
     in
-    aux [] 0 0
+    aux [] 0 0 |> fix_stuff
 
-  let remove_empty_tail t = 
-      List.drop_while t ~f:(function Entry.Empty _ -> true | Full _ -> false)
-
-
-  let pop_tail t =
-    let t = remove_empty_tail t
-    in
-    (List.last_exn t, List.drop_last_exn t)
+  let pop_tail t : Entry.t * t = (List.last_exn t, List.drop_last_exn t)
 
   let shuffle_last_block t =
     let last_block, t = pop_tail t in
@@ -46,9 +59,28 @@ module Entry_list = struct
       | Empty n :: tl ->
           if count = n then aux (Full (count, id) :: acc) 0 tl
           else if count > n then aux (Full (n, id) :: acc) (count - n) tl
-          else aux (Empty (n - count) :: Full (count - n, id) :: acc) 0 tl
+          else aux (Empty (n - count) :: Full (count, id) :: acc) 0 tl
     in
-    aux [] count t
+    aux [] count t |> fix_stuff
+
+  let shuffle_last_block' t =
+    let count, id, t =
+      List.fold t ~init:(-1, -1, []) ~f:(fun (count, id, acc) t ->
+          match t with
+          | Entry.Full (c, i) as e ->
+              if id <= 0 && i > 0 then (c, i, Entry.Empty c :: acc)
+              else (count, id, e :: acc)
+          | e -> (count, id, e :: acc))
+    in
+    let t = List.rev t in
+    let rec aux acc added = function
+      | [] -> List.rev acc
+      | (Entry.Full _ as hd) :: tl -> aux (hd :: acc) added tl
+      | Empty n :: tl ->
+          if (not added) && count > n then aux (Full (n, id) :: acc) true tl
+          else aux (Empty (n - count) :: Full (count, id) :: acc) added tl
+    in
+    aux [] false t |> fix_stuff
 
   let int_list t =
     List.map t ~f:(function
@@ -60,20 +92,31 @@ module Entry_list = struct
     List.foldi (int_list t) ~init:0 ~f:(fun i acc n ->
         (Option.value n ~default:0 * i) + acc)
 
-  let has_gap t = 
-      List.exists (remove_empty_tail t) ~f:(function 
-          | Empty _ -> true
-          | Full _ -> false)
+  let has_gap t =
+    List.exists (fix_stuff t) ~f:(function Empty _ -> true | Full _ -> false)
+
+  let part2done t =
+    List.for_all t ~f:(function Entry.Full (_, id) -> id <= 0 | _ -> false)
 end
 
 let part1 s =
-     let t = Entry_list.of_string s in
-     let rec aux t =
-         if Entry_list.has_gap t then
-             aux (Entry_list.shuffle_last_block t)
-         else Entry_list.checksum t
-     in
-     aux t |> Int.to_string |> Ok
-     
+  let t = Entry_list.of_string s in
+  let rec aux t =
+    if Entry_list.has_gap t then aux (Entry_list.shuffle_last_block t)
+    else Entry_list.checksum t
+  in
+  aux t |> Int.to_string |> Ok
 
-let part2 _ = Error (Error.of_string "Unimplemented")
+let part2 s =
+  let t = Entry_list.of_string s in
+  let rec aux t =
+    print_s [%sexp (Entry_list.int_list t : int option list)];
+    print_endline "";
+    if Entry_list.part2done t then
+      Entry_list.checksum
+        (List.map t ~f:(function
+          | Entry.Empty _ as e -> e
+          | Full (n, id) -> Full (n, -id)))
+    else aux (Entry_list.shuffle_last_block' t)
+  in
+  aux t |> Int.to_string |> Ok
