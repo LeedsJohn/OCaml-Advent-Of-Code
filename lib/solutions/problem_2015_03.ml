66b268538b36324714f5ac5244ecfaac244cfc39
John Leeds <leedsjohn6@gmail.com>
2024-12-29 17:19:52 -0600

    some 2015 stuff

---
+++ lib/solutions/problem_2015_03.ml
@@ -0,0 +1,33 @@
+open! Core
+open! Helpers
+
+let get_dir = function
+  | '>' -> (1, 0)
+  | '<' -> (-1, 0)
+  | 'v' -> (0, 1)
+  | '^' -> (0, -1)
+  | c -> raise_s [%message "wtf" (c : char)]
+
+let part1 s =
+  String.fold s
+    ~init:((0, 0), Set.singleton (module Coordinate) (0, 0))
+    ~f:(fun (pos, s) dir ->
+      let dir = get_dir dir in
+      let new_pos = Coordinate.add pos dir in
+      (new_pos, Set.add s new_pos))
+  |> snd |> Set.length |> Int.to_string |> Ok
+
+let part2 s =
+  let _, _, res =
+    String.foldi s
+      ~init:((0, 0), (0, 0), Set.singleton (module Coordinate) (0, 0))
+      ~f:(fun i (pos1, pos2, s) dir ->
+        let dir = get_dir dir in
+        if i % 2 = 0 then
+          let new_pos = Coordinate.add pos1 dir in
+          (new_pos, pos2, Set.add s new_pos)
+        else
+          let new_pos = Coordinate.add pos2 dir in
+          (pos1, new_pos, Set.add s new_pos))
+  in
+  Set.length res |> Int.to_string |> Ok
---
+++ lib/solutions/problem_2015_04.ml
@@ -0,0 +1,16 @@
+open! Core
+
+let find_prefix s ~prefix =
+  let rec aux i =
+    let s = Md5.digest_string (s ^ Int.to_string i) |> Md5.to_hex in
+    if String.is_prefix s ~prefix then i else aux (i + 1)
+  in
+  aux 1
+
+let part1 s = find_prefix s ~prefix:"00000" |> Int.to_string |> Ok
+let part2 s = find_prefix s ~prefix:"000000" |> Int.to_string |> Ok
+
+let%expect_test "md5" =
+  print_s [%sexp (Md5.digest_string "abcdef609043" |> Md5.to_hex : string)];
+  [%expect {| 000001dbbfa3a5c83a2d506429c7b00e |}];
+  ()
